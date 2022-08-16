da3da7416e3278490ab4c8366b281c97b617a869
Muhammad Umer Farooq <umer@lablnet.com>
2022-08-16 16:47:15 +0500

    fix class to className react style
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -8,26 +8,25 @@ class Home extends React.Component {
     render() {
 
         return (
-            <div class="mb-8">
-                <section class="">
-                    <div class="container px-5 py-24 mx-auto">
-                        <div class="text-center mb-20">
+            <div className="mb-8">
+                <section className="">
+                    <div className="container px-5 py-24 mx-auto">
+                        <div className="text-center mb-20">
                             <h1
-                                class="sm:text-3xl text-2xl font-medium text-center title-font mb-4"
+                                className="sm:text-3xl text-2xl font-medium text-center title-font mb-4"
                             >
                                 Type your city name below.
                             </h1>
                             <div className='col-span-1'>
                                 <CitySelector />
                             </div>
-                            <p class="lg:w-2/3 mx-auto leading-relaxed text-base mb-6">
+                            <p className="lg:w-2/3 mx-auto leading-relaxed text-base mb-6">
                                 Choose from list.
                             </p>
                         </div>
                     </div>
                 </section>
             </div>
-
         );
     }
 }
--- web/src/pages/Weather.js
+++ web/src/pages/Weather.js
@@ -49,72 +49,72 @@ class Weather extends React.Component {
 
         return (
             <div>
-                <div class="">
-                    <div class="flex flex-wrap">
-                        <div class="w-full px-2">
-                            <div class="bg-gray-900 text-white relative min-w-0 break-words rounded-lg overflow-hidden shadow-sm mb-4 w-full ">
-                                <div class="px-8 py-8 relative">
-                                    <div class="flex mb-4 justify-between items-center">
+                <div className="">
+                    <div className="flex flex-wrap">
+                        <div className="w-full px-2">
+                            <div className="bg-gray-900 text-white relative min-w-0 break-words rounded-lg overflow-hidden shadow-sm mb-4 w-full ">
+                                <div className="px-8 py-8 relative">
+                                    <div className="flex mb-4 justify-between items-center">
                                         <div>
-                                            <h5 class="mb-0 font-medium text-xl">{city + " "}</h5>
-                                            <h6 class="mb-0">{weather && weather.date}</h6><small>{weather && weather.currentCondition}</small>
+                                            <h5 className="mb-0 font-medium text-xl">{city + " "}</h5>
+                                            <h6 className="mb-0">{weather && weather.date}</h6><small>{weather && weather.currentCondition}</small>
                                         </div>
-                                        <div class="text-right">
-                                            <h3 class="font-bold text-4xl mb-0"><span>{weather && weather.temp}&deg;</span></h3>
+                                        <div className="text-right">
+                                            <h3 className="font-bold text-4xl mb-0"><span>{weather && weather.temp}&deg;</span></h3>
                                         </div>
                                     </div>
                                     <hr className="text-white border-1 mt-2 mb-3" />
-                                    <div class="block sm:flex justify-between items-center flex-wrap">
-                                        <div class="w-full sm:w-1/2">
+                                    <div className="block sm:flex justify-between items-center flex-wrap">
+                                        <div className="w-full sm:w-1/2">
                                         </div>
-                                        <div class="w-full sm:w-1/2">
-                                            <div class="flex mx-5 mb-2 justify-between items-center"><span>Surise/Sunset</span><small class="px-2 inline-block">{weather && weather.sunrise}&nbsp; {weather && weather.sunset}</small></div>
+                                        <div className="w-full sm:w-1/2">
+                                            <div className="flex mx-5 mb-2 justify-between items-center"><span>Surise/Sunset</span><small className="px-2 inline-block">{weather && weather.sunrise}&nbsp; {weather && weather.sunset}</small></div>
                                         </div>
                                     </div>
-                                    <div class="block sm:flex justify-between items-center flex-wrap">
-                                        <div class="w-full sm:w-1/2">
-                                            <div class="flex mb-2 justify-between items-center"><span>Temp</span><small class="px-2 inline-block">{weather && weather.high}&nbsp;&deg; - {weather && weather.low}&nbsp;&deg;</small></div>
+                                    <div className="block sm:flex justify-between items-center flex-wrap">
+                                        <div className="w-full sm:w-1/2">
+                                            <div className="flex mb-2 justify-between items-center"><span>Temp</span><small className="px-2 inline-block">{weather && weather.high}&nbsp;&deg; - {weather && weather.low}&nbsp;&deg;</small></div>
                                         </div>
-                                        <div class="w-full sm:w-1/2">
-                                            <div class="flex mx-5 mb-2 justify-between items-center"><span>Feels like</span><small class="px-2 inline-block">{weather && weather.feelLikeTemp}&nbsp;&deg;</small></div>
+                                        <div className="w-full sm:w-1/2">
+                                            <div className="flex mx-5 mb-2 justify-between items-center"><span>Feels like</span><small className="px-2 inline-block">{weather && weather.feelLikeTemp}&nbsp;&deg;</small></div>
                                         </div>
-                                        <div class="w-full sm:w-1/2">
-                                            <div class="flex mb-2 justify-between items-center"><span>Humidity</span><small class="px-2 inline-block">{weather && weather.humidity}</small></div>
+                                        <div className="w-full sm:w-1/2">
+                                            <div className="flex mb-2 justify-between items-center"><span>Humidity</span><small className="px-2 inline-block">{weather && weather.humidity}</small></div>
                                         </div>
-                                        <div class="w-full sm:w-1/2">
-                                            <div class="flex mx-5 mb-2 justify-between items-center"><span>Wind</span><small class="px-2 inline-block">{weather && weather.wind}&nbsp;
+                                        <div className="w-full sm:w-1/2">
+                                            <div className="flex mx-5 mb-2 justify-between items-center"><span>Wind</span><small className="px-2 inline-block">{weather && weather.wind}&nbsp;
 
                                                 {/* <span className='w-20 h-20' dangerouslySetInnerHTML={{ __html: weather && weather['Wind Directin'] }}> </span> */}
                                             </small></div>
                                         </div>
-                                        <div class="w-full sm:w-1/2">
-                                            <div class="flex mb-2 justify-between items-center"><span>UV Index</span><small class="px-2 inline-block">{weather && weather.uv_index}</small></div>
+                                        <div className="w-full sm:w-1/2">
+                                            <div className="flex mb-2 justify-between items-center"><span>UV Index</span><small className="px-2 inline-block">{weather && weather.uv_index}</small></div>
                                         </div>
-                                        <div class="w-full sm:w-1/2">
-                                            <div class="flex mx-5 mb-2 justify-between items-center"><span>Visibility</span><small class="px-2 inline-block">{weather && weather.VisibilityValue}&nbsp;&deg;</small></div>
+                                        <div className="w-full sm:w-1/2">
+                                            <div className="flex mx-5 mb-2 justify-between items-center"><span>Visibility</span><small className="px-2 inline-block">{weather && weather.VisibilityValue}&nbsp;&deg;</small></div>
                                         </div>
-                                        <div class="w-full sm:w-1/2">
-                                            <div class="flex mb-2 justify-between items-center"><span>Pressure</span><small class="px-2 inline-block">{weather && weather.pressure}</small></div>
+                                        <div className="w-full sm:w-1/2">
+                                            <div className="flex mb-2 justify-between items-center"><span>Pressure</span><small className="px-2 inline-block">{weather && weather.pressure}</small></div>
                                         </div>
-                                        <div class="w-full sm:w-1/2">
-                                            <div class="flex mx-5 mb-2 justify-between items-center"><span>Dew Point</span><small class="px-2 inline-block">{weather && weather.dewPoint}&nbsp;&deg;</small></div>
+                                        <div className="w-full sm:w-1/2">
+                                            <div className="flex mx-5 mb-2 justify-between items-center"><span>Dew Point</span><small className="px-2 inline-block">{weather && weather.dewPoint}&nbsp;&deg;</small></div>
                                         </div>
-                                        <div class="w-full sm:w-1/2">
+                                        <div className="w-full sm:w-1/2">
                                         </div>
-                                        <div class="w-full sm:w-1/2">
-                                            <div class="flex mx-5 mb-2 justify-between items-center"><span>Moon Phase</span><small class="px-2 inline-block">{weather && weather.moonPhase}&nbsp;</small></div>
+                                        <div className="w-full sm:w-1/2">
+                                            <div className="flex mx-5 mb-2 justify-between items-center"><span>Moon Phase</span><small className="px-2 inline-block">{weather && weather.moonPhase}&nbsp;</small></div>
                                         </div>
-                                        <div class="w-full sm:w-1/2">
+                                        <div className="w-full sm:w-1/2">
                                         </div>
                                     </div>
                                     <hr className="text-white border-1 mt-2 mb-3" />
-                                    <div class="container px-5 py-5 mx-auto">
+                                    <div className="container px-5 py-5 mx-auto">
                                         <h2 className='font-bold text-2xl mb-3'>Air Quality Index</h2>
-                                        <div class="text-center">
+                                        <div className="text-center">
                                             <div className={'w-[70px] h-[70px] bg-red rounded-full flex justify-center items-center  mx-auto mb-2 ' + airQualityColor}>
                                                 <p>{airQualityNumber}</p>
                                             </div>
-                                            <p class="text-center mx-auto mt-2 mb-2">
+                                            <p className="text-center mx-auto mt-2 mb-2">
                                                 <strong>{weather && weather.airQualityText}</strong>
                                             </p>
                                             <p className='text-center mb-3'>
