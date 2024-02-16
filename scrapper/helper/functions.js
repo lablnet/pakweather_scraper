1a46b8643aff0bb9b2bc3c8e611d227929b8755a
Muhammad Umer Farooq <umer@lablnet.com>
2024-02-16 15:27:21 +0500

    Add helper functions for latitude, longitude, datetime, file to set, and set to file operations
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

---
+++ scrapper/helper/functions.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+
+/**
+ * Get Latitude and Longitude from given URL.
+ * 
+ * @param {string} url - The URL.
+ * 
+ * @returns {Array} - The latitude and longitude.
+ * 
+ * @author Muhammad Umer Farooq <umer@lablnet.com>
+ * @since v1.0.0
+ */
+const getLatLong = (url) => {
+    const latlong = url.split("/")[6].split(",");
+    return [latlong[0].trim(), latlong[1].replace("?unit=c", '').trim()];
+}
+
+/**
+ * Get the datetime of GMT+5.
+ * 
+ * @returns {Date} - The date.
+ * 
+ * @author Muhammad Umer Farooq <umer@lablnet.com>
+ * @since v1.0.1
+ */
+const getDatetime = () => {
+    return new Date().toLocaleString("en-US", {timeZone: "Asia/Karachi"});
+}
+
+/**
+ * File to set.
+ * 
+ * @param {string} file - The file name.
+ * 
+ * @returns {Set} - The set.
+ * 
+ * @author Muhammad Umer Farooq <umer@lablnet.com>
+ * @since v1.0.0
+ */
+const fileToSet = (data, fileName) => {
+    const data = fs.readFileSync(file, 'utf-8');
+    const lines = data.split('\n');
+    return new Set(lines);
+}
+
+/**
+ * Set to file.
+ * 
+ * @param {Set} set - The set.
+ * @param {string} file - The file name.
+ * 
+ * @returns {void}
+ * 
+ * @author Muhammad Umer Farooq <umer@lablnet.com>
+ * @since v1.0.0
+ */
+const setToFile = (set, file) => {
+    const data = [...set].join('\n');
+    fs.writeFileSync(file, data);
+}
+
+
+module.exports = {
+    getDatetime,
+    fileToSet,
+    setToFile
+}
